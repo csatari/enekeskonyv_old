710abdec144134ad08b8c8abf5e621d3916bcdb4
jex <csatari2864@gmail.com>
2014-07-27 21:17:57 +0200

    Store első feltöltése

---
+++ store/js/enekpont.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Albert on 2014.07.26..
+ */
+var enekpont = {};
+
+enekpont.megjelenit = function(enekPont) {
+    return "<div class=\"enekpont\" id=\""+enekPont.id+"\">"+enekPont.cim+"</div>";
+};
