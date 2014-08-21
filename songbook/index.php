8008da5d2d1621ff1b7f3d0b49b2a5e6a666ee84
jex <csatari2864@gmail.com>
2014-08-21 21:46:38 +0200

    Offline énekeskönyv elkezdése
    Az énekeskönyv elkezdése, már benne van, hogy milyen énekeket akar majd
    letölteni (ez még feladat).
    A cél az, hogy az index.php-t generálja le majd a store, a többit csak
    hozzácsatolja, és az összeset egy zip-ben letölti.

--- songbook/index.php
+++ songbook/index.php
@@ -1,3 +1,11 @@
 <!--Az énekeskönyv így fog kinézni talán...-->
 <html>
+<head>
+    <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+    <script src="js/jquery-1.11.1.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+<span class="osszesEnek">1,2,3,4,5,6,7</span>
+</body>
 </html>
