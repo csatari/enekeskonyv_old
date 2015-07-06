32c1e60e9baef73458d46a02a2a992d07903157d
jex <csatari2864@gmail.com>
2015-07-06 22:59:41 +0200

    uploaded database

---
+++ server/enekeskonyv.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Hoszt: 127.0.0.1
+-- Létrehozás ideje: 2015. Júl 06. 22:58
+-- Szerver verzió: 5.5.32
+-- PHP verzió: 5.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Adatbázis: `enekeskonyv`
+--
+CREATE DATABASE IF NOT EXISTS `enekeskonyv` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `enekeskonyv`;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `language`
+--
+
+CREATE TABLE IF NOT EXISTS `language` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- A tábla adatainak kiíratása `language`
+--
+
+INSERT INTO `language` (`id`, `name`) VALUES
+(1, 'Magyar'),
+(2, 'English');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `song`
+--
+
+CREATE TABLE IF NOT EXISTS `song` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `version` int(11) NOT NULL DEFAULT '1',
+  `title` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `song` text COLLATE utf8_unicode_ci NOT NULL,
+  `sheet_music` text COLLATE utf8_unicode_ci NOT NULL,
+  `language` int(11) NOT NULL,
+  `other_languages` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `labels` text COLLATE utf8_unicode_ci NOT NULL,
+  `comment` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10 ;
+
+--
+-- A tábla adatainak kiíratása `song`
+--
+
+INSERT INTO `song` (`id`, `version`, `title`, `song`, `sheet_music`, `language`, `other_languages`, `labels`, `comment`) VALUES
+(1, 1, 'ez egy cím', 'ez meg egy leíró', 'na ez meg egy kotta', 1, '0', '', ''),
+(2, 1, 'ez egy keres', 'ez meg egy leíró', 'na ez meg egy kotta', 1, '0', '', ''),
+(3, 1, 'ez egy másik', 'ez meg egy leíró', 'na ez meg egy kotta', 1, '0', '', ''),
+(4, 1, 'ebben is benne van, hogy cím', 'ez meg egy leíró', 'na ez meg egy kotta', 1, '0', '', ''),
+(5, 1, 'ez egy cím', 'üöplokjhgf', '', 2, '0', '', ''),
+(6, 1, 'ez egy cím', 'üöplokjhgfÖÜÓŐÚÉÁŰÍ öüóőúéáűí ez most egy kicsit hosszabb lesz, remélem már kifolyik az ötven karakterből', '', 1, '0', '', ''),
+(7, 1, 'Példaének', 'Atyám két kezedben, te vagy a lakhatom...[a][v][r][b][/b][/r][/v][/a]', 'options stave-distance=30 space=10\ntabstave notation=true tablature=false key=C time=4/4', 2, NULL, '', ''),
+(8, 1, 'Ez egy másik példaének', 'asdasdasd', 'options stave-distance=30 space=10\ntabstave notation=true tablature=false key=C time=4/4', 1, '7', 'ez,egy,címke', 'Ez itt egy megjegyzés'),
+(9, 1, 'Hull a pelyhes fehér hó', '[a] A      /       D     A   D       A      E   A  [/a]\nHull a pelyhes fehér hó, jöjj el kedves Télapó!\n[a]A      /       D     A    D     A    E      A[/a]\nMinden gyermek várva vár, vidám ének hangja száll.\n[a]A      E      A      E   A     E     A    E[/a]\nVan zsákodban minden jó, piros alma, mogyoró,\n[a]A       A7       D      A    A      E        A    [/a]\nJöjj el hozzánk, várunk rád, kedves öreg Télapó.\n\nNagy szakállú Télapó jó gyermek barátja.\nCukrot, diót, mogyorót rejteget a zsákja.\nAmerre jár reggelig kis cipőcske megtelik,\nmegtölti a Télapó, ha üresen látja!', 'options stave-distance=30 space=10\ntabstave notation=true tablature=false key=C time=4/4', 1, '0', 'télapó,hó,fehér,gyerek,vidám', '');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `songbook`
+--
+
+CREATE TABLE IF NOT EXISTS `songbook` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `title` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
+
+--
+-- A tábla adatainak kiíratása `songbook`
+--
+
+INSERT INTO `songbook` (`id`, `userid`, `title`) VALUES
+(1, 12, 'Érdekes'),
+(2, 12, 'EZisöüóőú'),
+(3, 12, 'Valami új'),
+(4, 12, 'Hozzáadom'),
+(5, 12, 'Nev'),
+(6, 18, 'kitaláltam'),
+(8, 18, 'kitaláltam2'),
+(9, 18, 'ABC'),
+(10, 18, 'ASD'),
+(11, 18, '1 hónap múlva lesz a szülinapom!!!!!!!!!!!44négy'),
+(12, 18, '');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `themes`
+--
+
+CREATE TABLE IF NOT EXISTS `themes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `public` int(1) NOT NULL DEFAULT '1',
+  `creator` int(11) NOT NULL,
+  `theme` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'in json',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `firstname` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `pass` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='A felhasználók itt vannak eltárolva' AUTO_INCREMENT=21 ;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `session`, `lastname`, `firstname`, `username`, `email`, `pass`, `date`) VALUES
+(12, 'asd', 'Albert', 'Csat', 'abc3', 'asd@sad.hu', 'NocNbz6oZYtUI', '2015-06-29 09:31:17'),
+(15, '', 'Csatári', 'Albert', 'csatari2', 'albert.csatari@gmail.com', 'NoTYSsIs4GzBs', '0000-00-00 00:00:00'),
+(16, '', 'Csatári', 'Albert', 'asdasd', 'albert.csatari@gmail.com', 'NoSML2UrO5JCc', '0000-00-00 00:00:00'),
+(18, 'acd638df15eb3b7494f21d066ef05f7ca05f27ccdb600479c52c1adf2b8a8367', 'Csatári', 'Albert', 'csatari', 'csatari2864@gmail.com', 'NoTYSsIs4GzBs', '2015-07-03 08:44:40'),
+(19, '', 'Réti', 'Noémi', 'ananasliget', 'noemireti73@gmail.com', 'NoMqp.IKiwJbs', '2015-04-06 15:56:55'),
+(20, '619976ab557aab88dc58a51874adf144424585afb0dfd597082653281763f000', 'Varga ', 'Ferenc', 'korház22', 'ferenc@gmail.com', 'NouU/0WbQyy/w', '2015-07-03 17:33:19');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
